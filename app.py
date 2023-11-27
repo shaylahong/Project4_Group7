0fafe07b0f6623fb738f489c732e1b8d95a15267
shaylahong <seihaleak.hong@gmail.com>
2023-11-27 20:52:13 +1030

    update

---
+++ app.py
@@ -0,0 +1,138 @@
+from flask import Flask,request,render_template
+import pickle
+from datetime import datetime
+
+
+
+app=Flask(__name__)
+
+#loading the model
+model=pickle.load(open('Housing_Price','rb'))
+
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+
+
+@app.route('/',methods=['POST'])
+def predict():
+    try:
+         '''
+        Required input for machine learning model
+        1. price
+        2. bedrooms
+        3. bathrooms
+        4. sqft_living
+        5. sqft_lot
+        6. floors
+        7. waterfront
+        8. view
+        9. condition
+        10. sqft_above
+        11. sqft_basement
+        12. yr_built
+        13. yr_renovated
+        '''
+         #syntax-->  var_name=request.form['<name which in present in html form(index.html)>']
+         query_price=request.form['price']
+         query_bedrooms=request.form['bedrooms']
+         query_bathrooms=request.form['bathrooms']
+         query_sqft_living=request.form['sqft_living']
+         query_sqft_lot=request.form['sqft_lot']
+         query_floors=request.form['floors']
+         query_waterfront=request.form['waterfront']
+         query_view=request.form['view']
+         query_condition=request.form['condition']
+         query_sqft_above=request.form['sqft_above']
+         query_sqft_basement=request.form['sqft_basement'] #Categorical Data
+         query_yr_built=request.form['yr_built'] #Categorical Data
+         query_yr_renovated=request.form['yr_renovated'] #Categorical Data
+
+         if query_tradetime<query_constructiontime:
+             return render_template('index.html')
+
+
+         #For renovation condition
+         if query_renovationcondition=="renovationCondition_1":
+            renovationCondition_2=0
+            renovationCondition_3=0
+            renovationCondition_4=0
+         elif query_renovationcondition=="renovationCondition_2":
+            renovationCondition_2=1
+            renovationCondition_3=0
+            renovationCondition_4=0
+         elif query_renovationcondition=="renovationCondition_3":
+            renovationCondition_2=0
+            renovationCondition_3=1
+            renovationCondition_4=0
+         else:
+            renovationCondition_2=0
+            renovationCondition_3=0
+            renovationCondition_4=1
+
+
+        # For building structure
+         if query_buildingstructure=="buildingStructure_1":
+            buildingStructure_2=0
+            buildingStructure_3=0
+            buildingStructure_4=0
+            buildingStructure_5=0
+            buildingStructure_6=0
+         elif query_buildingstructure=="buildingStructure_2":
+            buildingStructure_2=1
+            buildingStructure_3=0
+            buildingStructure_4=0
+            buildingStructure_5=0
+            buildingStructure_6=0
+         elif query_buildingstructure=="buildingStructure_3":
+            buildingStructure_2=0
+            buildingStructure_3=1
+            buildingStructure_4=0
+            buildingStructure_5=0
+            buildingStructure_6=0
+         elif query_buildingstructure=="buildingStructure_4":
+            buildingStructure_2=0
+            buildingStructure_3=0
+            buildingStructure_4=1
+            buildingStructure_5=0
+            buildingStructure_6=0
+         elif query_buildingstructure=="buildingStructure_5":
+            buildingStructure_2=0
+            buildingStructure_3=0
+            buildingStructure_4=0
+            buildingStructure_5=1
+            buildingStructure_6=0
+         else:
+            buildingStructure_2=0
+            buildingStructure_3=0
+            buildingStructure_4=0
+            buildingStructure_5=0
+            buildingStructure_6=1
+        
+        # For elevator
+         if query_elevator=="elevator_0":
+            elevator_1=0
+         else:
+            elevator_1=1
+
+         model_data=[[query_tradetime,query_followers,query_square,query_livingroom,
+                    query_drawingroom,query_kitchen,query_bathroom,query_constructiontime,
+                    query_communityaverage,renovationCondition_2,renovationCondition_3,renovationCondition_4,
+                    buildingStructure_2,buildingStructure_3,buildingStructure_4,buildingStructure_5,
+                    buildingStructure_6,elevator_1]]
+        
+         result=model.predict(model_data)
+         x=float(result)
+         y="{:.3f}".format(x)
+
+         return render_template('index.html',results=y)
+
+
+    except ValueError:
+        return render_template('index.html')
+   
+
+if __name__=="__main__":
+    app.run(debug=True)
