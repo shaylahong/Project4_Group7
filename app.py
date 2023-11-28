554cf15995eed6895a9954fa75bd4dddd72e591b
shaylahong <seihaleak.hong@gmail.com>
2023-11-28 22:07:42 +1030

    update

--- app.py
+++ app.py
@@ -2,125 +2,380 @@ from flask import Flask,request,render_template
 import pickle
 from datetime import datetime
 
-
-
 app=Flask(__name__)
-
 #loading the model
-model=pickle.load(open('Housing_Price','rb'))
 
+with open('random_forest.pkl', 'rb') as file: 
+    random_forest_model = pickle.load(file)
 
 @app.route('/')
+
 def home():
     return render_template('index.html')
-
-
 
 @app.route('/',methods=['POST'])
 def predict():
     try:
          '''
         Required input for machine learning model
-        1. price
-        2. bedrooms
-        3. bathrooms
-        4. sqft_living
-        5. sqft_lot
-        6. floors
-        7. waterfront
-        8. view
-        9. condition
-        10. sqft_above
-        11. sqft_basement
-        12. yr_built
-        13. yr_renovated
+        1. bedrooms
+        2. bathrooms
+        3. sqft_living
+        4. sqft_lot
+        5. floors
+        6. yr_built
+        7. yr_renovated
         '''
          #syntax-->  var_name=request.form['<name which in present in html form(index.html)>']
-         query_bedrooms = float(re)
+         query_bedrooms = float(request.form['bedrooms'])
+         query_bathrooms = float(request.form['bathrooms'])
+         query_sqft_living = float(request.form['sqft_living'])
+         query_sqft_lot = float(request.form['sqft_lot'])
+         query_floors = float(request.form['floors'])
+         query_yr_built = float(request.form['yr_built'])
+         query_yr_renovated = float(request.form['yr_renovated'])
 
-         if query_tradetime<query_constructiontime:
-             return render_template('index.html')
-
-
-         #For renovation condition
-         if query_renovationcondition=="renovationCondition_1":
-            renovationCondition_2=0
-            renovationCondition_3=0
-            renovationCondition_4=0
-         elif query_renovationcondition=="renovationCondition_2":
-            renovationCondition_2=1
-            renovationCondition_3=0
-            renovationCondition_4=0
-         elif query_renovationcondition=="renovationCondition_3":
-            renovationCondition_2=0
-            renovationCondition_3=1
-            renovationCondition_4=0
+         #For Bedroom condition
+         #For Bedroom condition
+         if query_bedrooms=="numberofbedrooms":
+            numberofbedrooms = 1
+            numberofbedrooms_1=0
+            numberofbedrooms_2=0
+            numberofbedrooms_3=0
+            umberofbedrooms_4=0
+            numberofbedrooms_5=0
+            numberofbedrooms_6=0
+            numberofbedrooms_7=0
+            numberofbedrooms_8=0
+            numberofbedrooms_9=0
+         elif query_bedrooms=="numberofbedrooms_1":
+            numberofbedrooms=0
+            numberofbedrooms_1=1
+            numberofbedrooms_2=0
+            numberofbedrooms_3=0
+            umberofbedrooms_4=0
+            numberofbedrooms_5=0
+            numberofbedrooms_6=0
+            numberofbedrooms_7=0
+            numberofbedrooms_8=0
+            numberofbedrooms_9=0
+         elif query_bedrooms=="numberofbedrooms_2":
+            numberofbedrooms=0
+            numberofbedrooms_1=0
+            numberofbedrooms_2=1
+            numberofbedrooms_3=0
+            umberofbedrooms_4=0
+            numberofbedrooms_5=0
+            numberofbedrooms_6=0
+            numberofbedrooms_7=0
+            numberofbedrooms_8=0
+            numberofbedrooms_9=0
+         elif query_bedrooms=="numberofbedrooms_3":
+            numberofbedrooms=0
+            numberofbedrooms_1=0
+            numberofbedrooms_2=0
+            numberofbedrooms_3=1
+            umberofbedrooms_4=0
+            numberofbedrooms_5=0
+            numberofbedrooms_6=0
+            numberofbedrooms_7=0
+            numberofbedrooms_8=0
+            numberofbedrooms_9=0
+         elif query_bedrooms=="numberofbedrooms_4":
+            numberofbedrooms=0
+            numberofbedrooms_1=0
+            numberofbedrooms_2=0
+            numberofbedrooms_3=0
+            numberofbedrooms_4=1
+            numberofbedrooms_5=0
+            numberofbedrooms_6=0
+            numberofbedrooms_7=0
+            numberofbedrooms_8=0
+            numberofbedrooms_9=0
+         elif query_bedrooms=="numberofbedrooms_5":
+            numberofbedrooms=0
+            numberofbedrooms_1=0
+            numberofbedrooms_2=0
+            numberofbedrooms_3=0
+            numberofbedrooms_4=0
+            numberofbedrooms_5=1
+            numberofbedrooms_6=0
+            numberofbedrooms_7=0
+            numberofbedrooms_8=0
+            numberofbedrooms_9=0
+         elif query_bedrooms=="numberofbedrooms_6":
+            numberofbedrooms=0
+            numberofbedrooms_1=0
+            numberofbedrooms_2=0
+            numberofbedrooms_3=0
+            numberofbedrooms_4=0
+            numberofbedrooms_5=0
+            numberofbedrooms_6=1
+            numberofbedrooms_7=0
+            numberofbedrooms_8=0
+            numberofbedrooms_9=0
+         elif query_bedrooms=="numberofbedrooms_7":
+            numberofbedrooms=0
+            numberofbedrooms_1=0
+            numberofbedrooms_2=0
+            numberofbedrooms_3=0
+            numberofbedrooms_4=0
+            numberofbedrooms_5=0
+            numberofbedrooms_6=0
+            numberofbedrooms_7=1
+            numberofbedrooms_8=0
+            numberofbedrooms_9=0
+         elif query_bedrooms=="numberofbedrooms_8":
+            numberofbedrooms=0
+            numberofbedrooms_1=0
+            numberofbedrooms_2=0
+            numberofbedrooms_3=0
+            numberofbedrooms_4=0
+            numberofbedrooms_5=0
+            numberofbedrooms_6=0
+            numberofbedrooms_7=0
+            numberofbedrooms_8=1
+            numberofbedrooms_9=0
          else:
-            renovationCondition_2=0
-            renovationCondition_3=0
-            renovationCondition_4=1
+            numberofbedrooms=0
+            numberofbedrooms_1=0
+            numberofbedrooms_2=0
+            numberofbedrooms_3=0
+            numberofbedrooms_4=0
+            numberofbedrooms_5=0
+            numberofbedrooms_6=0
+            numberofbedrooms_7=0
+            numberofbedrooms_8=0
+            numberofbedrooms_9=1
 
+        # For Bathroom condition
+         if query_bathrooms=="numberofbathrooms":
+            numberofbathrooms=1
+            numberofbathrooms_1=0
+            numberofbathrooms_125=0
+            numberofbathrooms_150=0
+            numberofbathrooms_175=0
+            numberofbathrooms_2=0
+            numberofbathrooms_225= 0
+            numberofbathrooms_250= 0
+            numberofbathrooms_275= 0
+            numberofbathrooms_3= 0
+            numberofbathrooms_325=0
+            numberofbathrooms_350= 0
+            numberofbathrooms_375= 0
+            numberofbathrooms_4= 0
+            numberofbathrooms_425= 0
+            numberofbathrooms_450= 0
+            numberofbathrooms_475= 0
+            numberofbathrooms_5= 0
+            numberofbathrooms_520= 0
+            numberofbathrooms_550= 0
 
-        # For building structure
-         if query_buildingstructure=="buildingStructure_1":
-            buildingStructure_2=0
-            buildingStructure_3=0
-            buildingStructure_4=0
-            buildingStructure_5=0
-            buildingStructure_6=0
-         elif query_buildingstructure=="buildingStructure_2":
-            buildingStructure_2=1
-            buildingStructure_3=0
-            buildingStructure_4=0
-            buildingStructure_5=0
-            buildingStructure_6=0
-         elif query_buildingstructure=="buildingStructure_3":
-            buildingStructure_2=0
-            buildingStructure_3=1
-            buildingStructure_4=0
-            buildingStructure_5=0
-            buildingStructure_6=0
-         elif query_buildingstructure=="buildingStructure_4":
-            buildingStructure_2=0
-            buildingStructure_3=0
-            buildingStructure_4=1
-            buildingStructure_5=0
-            buildingStructure_6=0
-         elif query_buildingstructure=="buildingStructure_5":
-            buildingStructure_2=0
-            buildingStructure_3=0
-            buildingStructure_4=0
-            buildingStructure_5=1
-            buildingStructure_6=0
-         else:
-            buildingStructure_2=0
-            buildingStructure_3=0
-            buildingStructure_4=0
-            buildingStructure_5=0
-            buildingStructure_6=1
-        
-        # For elevator
-         if query_elevator=="elevator_0":
-            elevator_1=0
-         else:
-            elevator_1=1
+         elif query_bathrooms==" numberofbathrooms_1":
+            numberofbathrooms=0
+            numberofbathrooms_1=1
+            numberofbathrooms_125=0
+            numberofbathrooms_150=0
+            numberofbathrooms_175=0
+            numberofbathrooms_2=0
+            numberofbathrooms_225= 0
+            numberofbathrooms_250= 0
+            numberofbathrooms_275= 0
+            numberofbathrooms_3= 0
+            numberofbathrooms_325=0
+            numberofbathrooms_350= 0
+            numberofbathrooms_375= 0
+            numberofbathrooms_4= 0
+            numberofbathrooms_425= 0
+            numberofbathrooms_450= 0
+            numberofbathrooms_475= 0
+            numberofbathrooms_5= 0
+            numberofbathrooms_520= 0
+            numberofbathrooms_550= 0
+         elif query_bathrooms=="numberofbathrooms_125":
+            numberofbathrooms=0
+            numberofbathrooms_1=0
+            numberofbathrooms_125=1
+            numberofbathrooms_150=0
+            numberofbathrooms_175=0
+            numberofbathrooms_2=0
+            numberofbathrooms_225= 0
+            numberofbathrooms_250= 0
+            numberofbathrooms_275= 0
+            numberofbathrooms_3= 0
+            numberofbathrooms_325=0
+            numberofbathrooms_350= 0
+            numberofbathrooms_375= 0
+            numberofbathrooms_4= 0
+            numberofbathrooms_425= 0
+            numberofbathrooms_450= 0
+            numberofbathrooms_475= 0
+            numberofbathrooms_5= 0
+            numberofbathrooms_520= 0
+            numberofbathrooms_550= 0
 
-         model_data=[[query_tradetime,query_followers,query_square,query_livingroom,
-                    query_drawingroom,query_kitchen,query_bathroom,query_constructiontime,
-                    query_communityaverage,renovationCondition_2,renovationCondition_3,renovationCondition_4,
-                    buildingStructure_2,buildingStructure_3,buildingStructure_4,buildingStructure_5,
-                    buildingStructure_6,elevator_1]]
-        
-         result=model.predict(model_data)
-         x=float(result)
-         y="{:.3f}".format(x)
+         elif query_bathrooms==" numberofbathrooms_150":
+            numberofbathrooms=0
+            numberofbathrooms_1=0
+            numberofbathrooms_125=0
+            numberofbathrooms_150=1
+            numberofbathrooms_175=0
+            numberofbathrooms_2=0
+            numberofbathrooms_225= 0
+            numberofbathrooms_250= 0
+            numberofbathrooms_275= 0
+            numberofbathrooms_3= 0
+            numberofbathrooms_325=0
+            numberofbathrooms_350= 0
+            numberofbathrooms_375= 0
+            numberofbathrooms_4= 0
+            numberofbathrooms_425= 0
+            numberofbathrooms_450= 0
+            numberofbathrooms_475= 0
+            numberofbathrooms_5= 0
+            numberofbathrooms_520= 0
+            numberofbathrooms_550= 0
+         elif query_bathrooms==" numberofbathrooms_175":
+            numberofbathrooms=0
+            numberofbathrooms_1=0
+            numberofbathrooms_125=0
+            numberofbathrooms_150=0
+            numberofbathrooms_175=1
+            numberofbathrooms_2=0
+            numberofbathrooms_225= 0
+            numberofbathrooms_250= 0
+            numberofbathrooms_275= 0
+            numberofbathrooms_3= 0
+            numberofbathrooms_325=0
+            numberofbathrooms_350= 0
+            numberofbathrooms_375= 0
+            numberofbathrooms_4= 0
+            numberofbathrooms_425= 0
+            numberofbathrooms_450= 0
+            numberofbathrooms_475= 0
+            numberofbathrooms_5= 0
+            numberofbathrooms_520= 0
+            numberofbathrooms_550= 0
 
-         return render_template('index.html',results=y)
+         elif query_bathrooms==" numberofbathrooms_2":
+            numberofbathrooms=0
+            numberofbathrooms_1=0
+            numberofbathrooms_125=0
+            numberofbathrooms_150=0
+            numberofbathrooms_175=0
+            numberofbathrooms_2=1
+            numberofbathrooms_225= 0
+            numberofbathrooms_250= 0
+            numberofbathrooms_275= 0
+            numberofbathrooms_3= 0
+            numberofbathrooms_325=0
+            numberofbathrooms_350= 0
+            numberofbathrooms_375= 0
+            numberofbathrooms_4= 0
+            numberofbathrooms_425= 0
+            numberofbathrooms_450= 0
+            numberofbathrooms_475= 0
+            numberofbathrooms_5= 0
+            numberofbathrooms_520= 0
+            numberofbathrooms_550= 0
 
+         elif query_bathrooms==" numberofbathrooms_225":
+            numberofbathrooms=0
+            numberofbathrooms_1=0
+            numberofbathrooms_125=0
+            numberofbathrooms_150=0
+            numberofbathrooms_175=0
+            numberofbathrooms_2=0
+            numberofbathrooms_225= 1
+            numberofbathrooms_250= 0
+            numberofbathrooms_275= 0
+            numberofbathrooms_3= 0
+            numberofbathrooms_325=0
+            numberofbathrooms_350= 0
+            numberofbathrooms_375= 0
+            numberofbathrooms_4= 0
+            numberofbathrooms_425= 0
+            numberofbathrooms_450= 0
+            numberofbathrooms_475= 0
+            numberofbathrooms_5= 0
+            numberofbathrooms_520= 0
+            numberofbathrooms_550= 0
 
+         elif query_bathrooms==" numberofbathrooms_250":
+            numberofbathrooms=0
+            numberofbathrooms_1=0
+            numberofbathrooms_125=0
+            numberofbathrooms_150=0
+            numberofbathrooms_175=0
+            numberofbathrooms_2=0
+            numberofbathrooms_225= 0
+            numberofbathrooms_250= 1
+            numberofbathrooms_275= 0
+            numberofbathrooms_3= 0
+            numberofbathrooms_325=0
+            numberofbathrooms_350= 0
+            numberofbathrooms_375= 0
+            numberofbathrooms_4= 0
+            numberofbathrooms_425= 0
+            numberofbathrooms_450= 0
+            numberofbathrooms_475= 0
+            numberofbathrooms_5= 0
+            numberofbathrooms_520= 0
+            numberofbathrooms_550= 0
+
+         elif query_bathrooms==" numberofbathrooms_275":
+            numberofbathrooms=0
+            numberofbathrooms_1=0
+            numberofbathrooms_125=0
+            numberofbathrooms_150=0
+            numberofbathrooms_175=0
+            numberofbathrooms_2=0
+            numberofbathrooms_225= 0
+            numberofbathrooms_250= 0
+            numberofbathrooms_275= 1
+            numberofbathrooms_3= 0
+            numberofbathrooms_325=0
+            numberofbathrooms_350= 0
+            numberofbathrooms_375= 0
+            numberofbathrooms_4= 0
+            numberofbathrooms_425= 0
+            numberofbathrooms_450= 0
+            numberofbathrooms_475= 0
+            numberofbathrooms_5= 0
+            numberofbathrooms_520= 0
+            numberofbathrooms_550= 0
+            
+         elif query_bathrooms==" numberofbathrooms_3":
+            numberofbathrooms=0
+            numberofbathrooms_1=0
+            numberofbathrooms_125=0
+            numberofbathrooms_150=0
+            numberofbathrooms_175=0
+            numberofbathrooms_2=0
+            numberofbathrooms_225= 0
+            numberofbathrooms_250= 0
+            numberofbathrooms_275= 0
+            numberofbathrooms_3= 1
+            numberofbathrooms_325=0
+            numberofbathrooms_350= 0
+            numberofbathrooms_375= 0
+            numberofbathrooms_4= 0
+            numberofbathrooms_425= 0
+            numberofbathrooms_450= 0
+            numberofbathrooms_475= 0
+            numberofbathrooms_5= 0
+            numberofbathrooms_520= 0
+            numberofbathrooms_550= 0
+         
+         model_data = [[query_bedrooms, query_bathrooms, 
+                        query_sqft_living, query_sqft_lot, 
+                        query_floors, query_yr_built, query_yr_renovated]]
+         result=random_forest_model.predict(model_data)
+         predicted_price = float(result[0])
+         formatted_price = "{: .3f}".format(predicted_price)
+
+         return render_template('index.html',results= formatted_price)
     except ValueError:
         return render_template('index.html')
-   
-
 if __name__=="__main__":
     app.run(debug=True)
